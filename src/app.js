cbaf0864a5cfab7d002c3d647980e952033ffe72
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-12-11 16:26:33 +0700

    add .

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userRoutes from '../src/routes/user/userRoutes.js';
+import userRoutes from './routes/user/userRoutes.js';
 import http from 'http';
 
 dotenv.config();
